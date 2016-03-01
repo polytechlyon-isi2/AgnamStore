ef41973e2196b8821a192d8a86837ad3e2dd8aab
agory <gory.alexandre02@gmail.com>
2016-03-01 15:52:55 +0100

    Correction layout app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,7 +40,7 @@
         <div class="collapse navbar-collapse" id="app-navbar-collapse">
             <!-- Left Side Of Navbar -->
             <ul class="nav navbar-nav">
-                <li><a href="{{ url('/home') }}">Home <?php echo 'noisette';?></a></li>
+                <li><a href="{{ url('/') }}">Genre</a></li>
             </ul>
 
             <!-- Right Side Of Navbar -->
